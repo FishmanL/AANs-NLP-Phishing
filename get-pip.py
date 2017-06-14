f71defccf3d339b86215de31252654fa501d6e60
Leor <leor.fishman@gmail.com>
2017-06-14 09:46:22 -0400

    conversion to new code

--- get-pip.py
+++ get-pip.py
@@ -25,8 +25,8 @@ def testincrements(a, start, end, increment):
         return a
 
 # Load datasets.
-training_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=IRIS_TRAINING, features_dtype=np.int64, target_dtype=np.int64)
-test_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=IRIS_TEST, features_dtype=np.int64, target_dtype=np.int64)
+training_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=IRIS_TRAINING, features_dtype=np.float64, target_dtype=np.float64)
+test_set = tf.contrib.learn.datasets.base.load_csv_without_header(filename=IRIS_TEST, features_dtype=np.float64, target_dtype=np.float64)
 
 x_train, x_test, y_train, y_test = training_set.data, test_set.data, \
   training_set.target, test_set.target
@@ -49,60 +49,101 @@ ba=20
 # initializing relevant variables(ba=number of loops per avg f1, all else self explanatory)
 avgauc = 0
 avgf1 = 0
+avgprecision=0
+avgrecall=0
 ba = 20
 optimizerarraystart = [20, 20, 20]
 optimizerarraycurrent = [20, 20, 20];
 maxoptimizerarray = optimizerarraystart;
 maxavgf1 = 0
+
+
+def model_fn(features, targets, mode, params):
+
+  """Model function for Estimator."""
+
+  # Connect the first hidden layer to input layer
+  # (features) with relu activation
+  first_hidden_layer = tf.contrib.layers.relu(features, 20)
+
+
+  # Connect the second hidden layer to first hidden layer with relu
+  second_hidden_layer = tf.contrib.layers.relu(first_hidden_layer, 20)
+
+  third_hidden_layer = tf.contrib.layers.relu(second_hidden_layer, 20)
+
+  # Connect the output layer to second hidden layer (no activation fn)
+  output_layer = tf.contrib.layers.linear(second_hidden_layer, 2)
+
+  # Reshape output layer to 1-dim Tensor to return predictions
+  labels = tf.one_hot(indices=tf.cast(targets, tf.int32), depth=2)
+  predictions = output_layer
+
+  # Calculate loss using mean squared error
+  weight = tf.multiply(4.0, tf.cast(tf.equal(labels, 1), tf.float32)+1)
+
+  loss = tf.losses.mean_squared_error(labels, predictions, weights=weight)
+
+  # Calculate root mean squared error as additional eval metric
+  eval_metric_ops = {
+      "rmse":
+          tf.metrics.root_mean_squared_error(
+              tf.cast(labels, tf.float64), predictions),
+      "recall":
+        tf.metrics.recall(labels, predictions, weights=weight),
+      "precision":
+        tf.metrics.precision(labels, predictions, weights=weight),
+      "auc":
+        tf.metrics.auc(labels, predictions, weights=weight),
+      "tp":
+          tf.metrics.true_positives(labels, predictions, weights=weight),
+      "fn":
+          tf.metrics.false_negatives(labels, predictions, weights=weight),
+      "fp":
+          tf.metrics.false_positives(labels, predictions, weights=weight)
+
+  }
+
+  train_op = tf.contrib.layers.optimize_loss(
+      loss=loss,
+      global_step=tf.contrib.framework.get_global_step(),
+      learning_rate=.005,
+      optimizer="SGD")
+
+  from tensorflow.contrib.learn import ModelFnOps
+  return ModelFnOps(
+      mode=mode,
+      predictions=predictions,
+      loss=loss,
+      train_op=train_op,
+      eval_metric_ops=eval_metric_ops)
 # Build multilayer DNN with current array of layers and nodes, set <x to 6 on server.
 while len(optimizerarraycurrent)==3:
     for i in range(ba):
         # create model
 
-        classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=optimizerarraycurrent, n_classes=2)
-
+        #classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns, hidden_units=optimizerarraycurrent, n_classes=2)
+        nn = tf.contrib.learn.Estimator(model_fn=model_fn)
+        print("Hi")
         # Fit model.
-        def get_test_inputs():
-            x = tf.constant(x_train)
-            y = tf.constant(y_train)
+        #classifier.fit(x=x_train, y=y_train, steps=1000)
+        nn.fit(x=x_train, y=y_train, steps=1000)
+        ev = nn.evaluate(x=x_test, y=y_test, steps=1)
+        print("Loss: %s" % ev["loss"])
+        print("Precision: %s" % ev["precision"])
+        print("Recall: %s" % ev["recall"])
+        print("Auc: %s" % ev["auc"])
+        print("TP: %s" % ev["tp"])
+        print("FP: %s" % ev["fp"])
+        print("FN: %s" % ev["fn"])
 
-            return x, y
-        classifier.fit(x=x_train, y=y_train, steps=1000)
-        y = list(classifier.predict(x_test))
-        # Evaluate accuracy, first by printing predicted and actual labelings for the test set and then by calculating f1
-        #print(str(y))
-        #print(y_test)
-        # actually getting the f1 score
-        tp = float(0.0)
-        tn = float(0.0)
-        fn = float(0.0)
-        fp = float(0.0)
-        for c in range(len(y_test)):
-            if y_test[c] == 1 and y[c] == 1:
-                tp += 1
-            if y_test[c] == 0 and y[c] == 1:
-                fp += 1
-            if y_test[c] == 1 and y[c] == 0:
-                fn += 1
-            if y_test[c] == 0 and y[c] == 0:
-                tn += 1
-        print(tp, fp, tn, fn)
-        if tp + fp == 0 or tp + fn == 0:
-            precision = 0
-            recall = 0
-            f1 = 0
-        else:
-            precision = tp / (tp + fp)
-            recall = tp / (tp + fn)
-            f1 = 2 * (precision * recall) / (precision + recall)
-        avgf1 += f1
-
-        print("F1: " + str(f1))
-        print("Precision : " + str(precision))
-        print("Recall : " + str(recall))
     # at this point the program calculates the average f1 and auc for this setup of layers and nodes
     avgf1 = avgf1 / ba
+    avgprecision = avgprecision / ba
+    avgrecall = avgrecall / ba
     print("Average f1: " + str(avgf1))
+    print("Average precision(Fraction of emails marked as nonspam that were nonspam): " + str(avgprecision))
+    print("Average recall(fraction of emails that are nonspam marked as nonspam): " + str(avgrecall))
     # here's the maximization code
     if avgf1 > maxavgf1:
         maxoptimizerarray = optimizerarraycurrent
